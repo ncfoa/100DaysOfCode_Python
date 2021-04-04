3d4f8d93087195f2e3da691b60e107ecfd3a1000
Dave Beidle <dbeidle@gmail.com>
2021-04-04 14:01:39 -0500

    Completed Day 13 Debugging project

---
+++ 100daysOfCode/013/debug_odd_even.py
@@ -0,0 +1,16 @@
+# Fix the below code:
+
+# number = int(input("Which number do you want to check?" ))
+#
+# if number % 2 = 0:
+#   print("This is an even number.")
+# else:
+#   print("This is an odd number.")
+
+# Fixed:
+number = int(input("Which number do you want to check? "))
+
+if number % 2 == 0:
+    print("This is an even number.")
+else:
+    print("This is an odd number.")
