af2335c37c2150083649cb25872d22ea7484c06d
Dave Beidle <dbeidle@gmail.com>
2021-04-24 18:16:02 -0500

    Refactored Password generator with some error handling. Day 23 of 100.

--- 029/password_gen/main.py
+++ 029/password_gen/main.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import messagebox as mw
 import random
+import json
 
 
 # ---------------------------- Password Generator ------------------------------- #
@@ -10,7 +11,7 @@ def generate_password():
             'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
             'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!',
             '#', '$', '%', '&', '*', '@']
-    print(len(char))
+
     for i in range(10):
         num = random.randint(0, len(char) - 1)
         pw.append(char[num])
@@ -22,25 +23,81 @@ def generate_password():
     return pw
 
 
+# ---------------------------- Search Password ------------------------------- #
+def search():
+    site = url_entry.get()
+
+    if not site:
+        mw.showwarning(message="Required Field missing.", parent=w, detail="Please ensure you have put in the Website "
+                                                                           "Name.")
+    else:
+        try:
+            with open("data.json", "r") as r:
+                data = json.load(r)
+
+        except KeyError:
+            mw.showwarning(message=f"No entry for {site}.", parent=w,
+                           detail=f"You do not have an entry for {site} in this application")
+        except FileNotFoundError:
+            mw.showwarning(message=f"Data File Not Found.", parent=w,
+                           detail=f"The data file does not exist.")
+            r.close()
+
+        else:
+            email_entry.delete(0, END)
+            email_entry.insert(0, data[site]["email"])
+            password_entry.delete(0, END)
+            password_entry.insert(0, data[site]["password"])
+            w.clipboard_clear()
+            w.clipboard_append(data[site]["password"])
+
+
 # ---------------------------- Save Password ------------------------------- #
 def save_password():
     web = url_entry.get()
     em = email_entry.get()
     pw = password_entry.get()
-    print(web)
+    new_data = {}
     if not url or not email or not pw:
         mw.showwarning(message="Please fill in all fields.", parent=w, detail="You left one or more fields blank.")
     else:
-        is_ok = mw.askyesno(title="My Title", message=f"{web}", detail=f"You entered: \n Username: {em} \n Password:"
-                                                                       f" {pw} \n Is this ok?")
-        if is_ok:
-            with open("./passwords.txt", "a") as f:
-                f.write(f"{web} | {em} | {pw} \n")
+        try:
+            with open("data.json", "r") as f:
+                data = json.load(f)
+
+        except FileNotFoundError:
+            f.close()
+            with open("data.json", "w") as f:
+                json.dump({}, f)
                 f.close()
 
-            url_entry.delete(0, END)
-            email_entry.delete(0, END)
-            password_entry.delete(0, END)
+        with open("data.json", "r") as f:
+            data = json.load(f)
+            f.close()
+
+            exists = {}
+        try:
+            if data[web]:
+                exists = data
+        except KeyError:
+            pass
+
+        if exists:
+            overwrite_ok = mw.askyesno(title="Continue?", message=f"{web} already has an entry",
+                                       detail=f"Username: {exists[web]['email']}\n Password: {exists[web]['password']}\n"
+                                              f" Do you want to overwrite?")
+            if overwrite_ok:
+                data.update(new_data)
+        else:
+            data.update(new_data)
+
+        with open("data.json", "w") as f:
+            json.dump(data, f, indent=4)
+            f.close()
+        url_entry.delete(0, END)
+        email_entry.delete(0, END)
+        password_entry.delete(0, END)
+
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -56,16 +113,18 @@ url = Label(text="Website: ")
 url.grid(column=0, row=3, sticky="E")
 url_entry = Entry(width=35)
 url_entry.focus()
-url_entry.grid(column=1, row=3, columnspan=2, sticky="E")
+url_entry.grid(column=1, row=3, sticky="E")
+search_btn = Button(text="Search", width=21, command=search)
+search_btn.grid(column=2, row=3)
 email = Label(text="Email/Username: ")
 email.grid(column=0, row=4, sticky="E")
 email_entry = Entry(width=35)
-email_entry.grid(column=1, row=4, columnspan=2, sticky="E")
+email_entry.grid(column=1, row=4, columnspan=2, sticky="WE")
 password = Label(text="Password: ")
 password.grid(column=0, row=5, sticky="E")
-password_entry = Entry(width=21)
-password_entry.grid(column=1, row=5, sticky="W")
-pass_btn = Button(text="Generate Password", command=generate_password)
+password_entry = Entry(width=35)
+password_entry.grid(column=1, row=5, columnspan=2, sticky="W")
+pass_btn = Button(text="Generate Password", width=21, borderwidth=4, relief="ridge", command=generate_password)
 pass_btn.grid(column=2, row=5)
 add_btn = Button(text="Add", command=save_password)
 add_btn.grid(column=1, row=6, columnspan=2, sticky="EW")
