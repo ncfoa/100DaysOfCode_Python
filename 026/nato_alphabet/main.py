8f79ba16e8b15deb056b6a246516662178fb3843
Dave Beidle <dbeidle@gmail.com>
2021-04-25 12:06:17 -0500

    Completed Day 24 of 100, Course day 30. Refactored nato_alphabet.py for error handling.

--- 026/nato_alphabet/main.py
+++ 026/nato_alphabet/main.py
@@ -5,7 +5,15 @@ alphabet = {"a": "alpha", "b": "bravo", "c": "charlie", "d": "delta", "e": "echo
             "o": "oscar", "p": "papa", "q": "quebec", "r": "romeo", "s": "sierra", "t": "tango", "u": "uniform",
             "v": "victor", "w": "whiskey", "x": "xray", "y": "yankee", "z": "zulu", " ": " "}
 
-# word should be any word that you need to spell out phonetically ie: Supercalifragilisticexpialidocious
-word = input("What word do you need to spell out? ").lower()
-print([alphabet[letter] for letter in word])
 
+# word should be any word that you need to spell out phonetically ie: Supercalifragilisticexpialidocious
+def phonetic():
+    word = input("What word do you need to spell out? ").lower()
+    try:
+        print([alphabet[letter] for letter in word])
+    except KeyError:
+        print("Numbers and Symbols are not a valid input.")
+        phonetic()
+
+
+phonetic()
---
+++ 030/README.md
@@ -0,0 +1,16 @@
+# 100DaysofCode_Python Day 30
+
+The Day 30 class was on implementing Error handling into 2 existing projects. I will list them below. 
+
+While I could have just implemented them in this folder, I felt that it was best to update the existing repo with the current files. You can look at the changelog to see the modifications made. 
+
+
+# Password Generator from day 29
+[Link to 029](https://github.com/dbeidle/100DaysOfCode_Python/tree/main/029/password_gen)
+
+
+# Nato Alphabet from day 26
+[Link to 026](https://github.com/dbeidle/100DaysOfCode_Python/tree/main/026/nato_alphabet
+)
+
+
