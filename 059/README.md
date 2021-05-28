4ec5e3ce05f641213e9d0769cbf4d7f999e2e0c0
Dave Beidle <dbeidle@gmail.com>
2021-05-28 07:22:19 -0500

    Completed day 58 of course. Day 59 of 100. Adding bootstrap to previously used blog site.

---
+++ 059/README.md
@@ -0,0 +1,5 @@
+# 100 Days of Code Python
+
+### Bootstrap Blog Site
+This day's code required [Bootstrap Clean Blog Template](https://startbootstrap.com/previews/clean-blog/) 
+We used this to upgrade the blog instance from day 57 course work. 
---
+++ 059/post.py
@@ -0,0 +1,8 @@
+class Post:
+    def __init__(self, id, date, title, subtitle, author, body):
+        self.id = id
+        self.date = date
+        self.title = title
+        self.body = body
+        self.subtitle = subtitle
+        self.author = author
