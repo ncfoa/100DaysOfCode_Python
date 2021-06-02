0ede6fce4b9070e824bb5a1b09de909e580e2f44
Dave Beidle <dbeidle@gmail.com>
2021-06-02 06:53:53 -0500

    Fixed some bugs in tinder_bot

--- 050/tinder_bot/main.py
+++ 050/tinder_bot/main.py
@@ -11,12 +11,15 @@ chrome_driver_path = "../../chromedriver"
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
 driver.get(url=URL)
 sleep(5)
-driver.maximize_window()
-driver.find_element_by_xpath('//*[@id="t1197069193"]/div/div[2]/div/div/div[1]/button').click()
-driver.find_element_by_xpath('//*[@id="t1197069193"]/div/div[1]/div/main/div[1]/div/div/div/div/header/'
-                             'div/div[2]/div[2]/a').click()
+# driver.maximize_window()
+# Press login button
+login_button = driver.find_element_by_xpath('//*[@id="c-174738105"]/div/div[1]/div/main/div[1]/div/div/div/div/header/div/div[2]/div[2]/a')
+login_button.click()
+# Get rid of cookie banner
+
 sleep(2)
-driver.find_element_by_xpath('//*[@id="t-531311883"]/div/div/div[1]/div/div[3]/span/div[2]/button').click()
+# Login with facbook pop-up
+driver.find_element_by_xpath('//*[@id="c-1903119181"]/div/div/div[1]/div/div[3]/span/div[2]/button').click()
 sleep(2)
 base_window = driver.window_handles[0]
 fb_login_window = driver.window_handles[1]
@@ -25,21 +28,30 @@ sleep(2)
 driver.find_element_by_id("email").send_keys(CONFIG["EMAIL"])
 driver.find_element_by_id("pass").send_keys(CONFIG["PASSWD"])
 driver.find_element_by_id("pass").submit()
-# driver.find_element_by_name("__CONFIRM__").click()
 sleep(2)
+# Return to main window
 driver.switch_to.window(base_window)
+# Get rid of useless pop-ups
+sleep(5)
+allow_location_button = driver.find_element_by_xpath('//*[@id="c-1903119181"]/div/div/div/div/div[3]/button[1]')
+
+allow_location_button.click()
+notifications_button = driver.find_element_by_xpath('//*[@id="c-1903119181"]/div/div/div/div/div[3]/button[2]')
+notifications_button.click()
+cookies = driver.find_element_by_xpath('//*[@id="c-174738105"]/div/div[2]/div/div/div[1]/button')
+cookies.click()
+print("Start sleep")
+sleep(45)
+print("end sleep")
+like_you = driver.find_element_by_xpath('//*[@id="c-1903119181"]/div/div/div/div[3]/button[2]')
+like_you.click()
 count = 0
-sleep(2)
-driver.find_element_by_xpath('//*[@id="t-531311883"]/div/div/div/div/div[3]/button[1]').click()
-sleep(1)
-driver.find_element_by_xpath('//*[@id="t-531311883"]/div/div/div/div/div[3]/button[2]').click()
-sleep(30)
 
 
 def window_click():
     sleep(1)
-    driver.find_element_by_xpath('//*[@id="t1197069193"]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div[2]'
-                                 '/div[4]/button').click()
+    driver.find_element_by_xpath('//*[@id="c-174738105"]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div[2]/div[4]/button').click()
+    sleep(1)
 
 
 def shut_down():
@@ -48,28 +60,30 @@ def shut_down():
     print(f"Swiped right on {count} profiles. Good Luck!")
 
 
-while (driver.find_element_by_xpath('//*[@id="t1197069193"]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/'
-                                    'div[2]/div[4]/button')):
+for n in range(100):
+
     sleep(1)
-    # driver.find_element_by_xpath('//*[@id="t1527440394"]/div/div/div[1]/div/div[4]/button').click()
+
     try:
         window_click()
-        if driver.find_element_by_css_selector('h3.Fz($ms).Fw($semibold).C(#fff)').text == "Get Tinder Plus®":
-            shut_down()
-        # Catches the cases where there is a "Matched" pop-up in front of the "Like" button:
+
     except ElementClickInterceptedException:
         print("errored")
+        if driver.find_element_by_css_selector('# c-1903119181 > div > div > div.Bg\(\$green-gradient-radial\).CenterAlign.D\(f\).Fxd\(c\) > div.W\(100\%\).Ta\(c\).Mt\(24px\).Mt\(12px\)--s.Mt\(8px\)--xs > h3'):
+            shut_down()
+        # Catches the cases where there is a "Matched" pop-up in front of the "Like" button:
         try:
-            driver.find_element_by_xpath('//*[@id="t1527440394"]/div/div/div[1]/div/div[4]/button').click()
-            print("try")
+            driver.find_element_by_xpath('//*[@id="c155633096"]/div/div/div[1]/div/div[4]/button').click()
         # Catch Add to home screen pop-up
         except NoSuchElementException:
             try:
-                print("first exception")
-                driver.find_element_by_xpath('//*[@id="t-531311883"]/div/div/div[2]/button[2]').click()
-        # Catches the cases where the "Like" button has not yet loaded, so wait 2 seconds before retrying.
-            except NoSuchElementException:
-                print("second exception")
-                sleep(2)
+                driver.find_element_by_xpath('//*[@id="c-1903119181"]/div/div/div[2]/button[2]').click()
+                window_click()
 
+            except NoSuchElementException:
+                sleep(2)
+                window_click()
+    # c-1903119181 > div > div > div.Bg\(\$green-gradient-radial\).CenterAlign.D\(f\).Fxd\(c\) > div.W\(100\%\).Ta\(c\).Mt\(24px\).Mt\(12px\)--s.Mt\(8px\)--xs > h3
     count += 1
+
+shut_down()
